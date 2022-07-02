e405d5995b9d6d3e664f48da28d96f2cedd49f1c
omsupal <supalomkar225@gmail.com>
2022-07-02 18:23:03 +0530

    first commit

---
+++ src/main/java/com/umrahme/umrah/ListingPage.java
@@ -0,0 +1,161 @@
+package com.umrahme.umrah;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ListingPage {
+		public WebDriver driver;
+		@FindBy(id="RouteCode-error")
+		private WebElement sel_dest_error;
+		
+		@FindBy(xpath="//small[@id='meccaCityNight-error']")
+		private WebElement meccastay_error;
+		
+		@FindBy(id="Nationality-error")
+		private WebElement nationality_error;
+		
+		@FindBy(xpath="//*[@id=\"frmHomeSearch\"]/div/div/div[1]/span") 
+		private WebElement select_destination;
+		
+		@FindBy(xpath="//input[@class='select2-search__field']")
+		private WebElement selctdest_search;
+		
+		@FindBy(id="StartDate") 
+		private WebElement startdate;
+
+		@FindBy(xpath="//span[@aria-labelledby='select2-meccaCityNight-container']") 
+		private WebElement makkah_stay;
+		
+		@FindBy(xpath="//input[@class='select2-search__field']")
+		private WebElement mecca_search;
+		
+		@FindBy(xpath="//span[@aria-labelledby='select2-madinaCityNight-container']")
+		WebElement madina_stay;
+
+		@FindBy(xpath="//div[@class='all-traveller show-occupancy']")
+		private WebElement travellers_rooms;
+
+		@FindBy(xpath="//label[contains(.,'Adults')]/preceding-sibling::select")
+		private WebElement adult_select;
+
+		@FindBy(xpath="//label[contains(.,'Children')]/preceding-sibling::select")
+		private WebElement child_select;
+		
+		private WebElement child_age_select;
+
+		
+		public WebElement getAge(int i) {
+			String str = "//ul[@class=\"age-wrap\"]/li["+i+"]/select";
+			child_age_select = driver.findElement(By.xpath(str));
+			return child_age_select;
+		}
+//		
+		@FindBy(xpath="//span[@aria-labelledby='select2-CountryOfResidence-container']/parent::span/parent::span") 
+		private WebElement countryofresidence;
+		
+		@FindBy(xpath="//input[@aria-controls='select2-CountryOfResidence-results']")
+		private WebElement searchCon;
+//
+		@FindBy(xpath="//span[@aria-labelledby='select2-Nationality-container']/parent::span/parent::span") 
+		private WebElement nationality;
+		
+		@FindBy(xpath="//input[@aria-controls='select2-Nationality-results']")
+		private WebElement search_nationality;
+
+		@FindBy(id="btnSearchPackage") 
+		private WebElement searchbtn;
+		
+		public WebElement getSelectDestinationerror(){
+
+			return sel_dest_error;
+		}
+		
+		public WebElement getmeccaerror(){
+
+			return meccastay_error;
+		}
+		
+		public WebElement getSelectNationalityerror(){
+
+			return nationality_error;
+		}
+
+		public WebElement getSelectDestination(){
+
+			return select_destination;
+		}
+		
+		public WebElement getSearchBox() {
+			return selctdest_search;
+		}
+
+		public WebElement getStartDate(){
+
+			return startdate;
+		}
+		
+		public WebElement getMakkahStay(){
+
+			return makkah_stay;
+		}
+		
+		public WebElement getMeccaSearch() {
+			return mecca_search;
+		}
+		
+		public WebElement getMadinaStay(){
+
+			return madina_stay;
+		}
+	
+		public WebElement getTravellersRooms(){
+
+			return travellers_rooms;
+		}
+
+		public WebElement getAdults(){
+
+			return adult_select;
+		}
+
+		public WebElement getChilds(){
+
+			return child_select;
+		}
+		
+
+		public WebElement getCountryOfResidence(){
+
+			return countryofresidence;
+		}
+		
+		public WebElement getSearchCON() {
+			return searchCon;
+		}
+
+		public WebElement getNationality(){
+
+			return nationality;
+		}
+		
+		public WebElement getSearchNationality(){
+
+			return search_nationality;
+		}
+	
+		public WebElement getSearch(){
+
+			return searchbtn;
+		}
+		
+		public ListingPage(WebDriver driver) {
+			this.driver=driver;
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			PageFactory.initElements(driver, this); // pagefactory.initElements is use to initialize the @FindBy
+		}
+				
+}
---
+++ src/test/java/com/umrahme/umrah/TestListing.java
@@ -0,0 +1,126 @@
+package com.umrahme.umrah;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+
+public class TestListing {
+	private static WebDriver driver;
+	private static ListingPage listing;
+	private static int ch = 2;
+
+	@Test
+	public void SelectDestinationerrorfetch() {
+		String str = listing.getSelectDestinationerror().getText();
+		System.out.println(str);
+	}
+
+	@Test
+	public void SelectNationalityerrorfetch() {
+		String str = listing.getSelectNationalityerror().getText();
+		System.out.println(str);
+	}
+
+	@Test
+	public void SelectDestination() {
+		listing.getSelectDestination().click();
+		listing.getSearchBox().sendKeys("Makkah Only Package", Keys.ENTER);
+	}
+
+	@Test
+	public void meccaerror() {
+		System.out.println(listing.getmeccaerror().getText());
+	}
+	
+	@Test
+	public void SelectDate() {
+		listing.getStartDate().click();
+		listing.getStartDate().clear();
+		listing.getStartDate().sendKeys("07-07-2022", Keys.ENTER);
+	}
+	
+	@Test
+	public void SelectmeccaStay() {
+		listing.getMakkahStay().click();
+		listing.getMeccaSearch().sendKeys("5", Keys.ENTER);
+	}
+	
+	@Test
+	public void clickOnTravellers() {
+		listing.getTravellersRooms().click();
+		this.selectAdult();
+		
+	}
+	
+	@Test
+	public void selectAdult() {
+		Select adult = new Select(listing.getAdults());
+		adult.selectByVisibleText("4");
+		this.selectChild();
+		
+	}
+	
+	@Test
+	public void selectChild() {
+		int i = 2;
+		Select child = new Select(listing.getChilds());
+		child.selectByVisibleText(""+i+"");
+		this.selectage();
+		
+	}
+	
+	@Test
+	public void selectage() {
+		for (int i = 1; i <= ch; i++) {
+			Select age = new Select(listing.getAge(i));
+			age.selectByIndex(3);
+		}
+	}
+	
+	@Test
+	public void selectCountryOfResidence() {
+		
+		listing.getCountryOfResidence().click();
+		listing.getSearchCON().sendKeys("United States Of America", Keys.ENTER);
+		
+	}
+	
+	@Test
+	public void selectNationality() {
+		listing.getNationality().click();
+		listing.getSearchNationality().sendKeys("United States Of America", Keys.ENTER);
+		
+	}
+	
+	@AfterTest
+	public void clickOnSearch() {
+		try {
+		listing.getSearch().click();
+		}catch (Exception e) {
+			System.out.println("Exception Occured Because of version mismatch");
+		}
+	}
+
+	@BeforeSuite
+	public void driverInitialize() throws Exception {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.umrahme.com/home/en-sa");
+		listing = new ListingPage(driver);
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		driver.quit();
+	}
+
+}
